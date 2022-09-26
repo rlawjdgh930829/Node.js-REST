26a88afd8df9298f91b301f5e4a625b9778a56ae
Kim Jeongho <kimjeongho930829@gmail.com>
2022-09-26 16:31:25 +0900

    crue 테스트

--- app.js
+++ app.js
@@ -1,19 +1,63 @@
+const path = require('path');
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const feedRouters = require('./routers/feed');
+const feedRoutes = require('./routes/feed');
+const mongoose = require('mongoose');
+const multer = require('multer');
 
 const app = express();
 
-app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Orogin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  },
 });
 
-app.use('/feed', feedRouters);
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
-app.listen(8080);
+app.use(bodyParser.json());
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
+mongoose
+  .connect(process.env.DB_URI)
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
